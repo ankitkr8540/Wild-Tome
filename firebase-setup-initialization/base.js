25ca2297f09e241d8f9d7a72dc317c907391399f
Antara <tewaryantara21@gmail.com>
2022-06-12 15:18:43 +0530

    added firebase settings

---
+++ firebase-setup-initialization/base.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase';
+import 'firebase/storage';
+export const app = firebase.initializeApp({
+  projectId: 'wild-tome-1',
+  appId: '1:958141639770:web:fc6f79f5888b2f1b19bc63',
+  storageBucket: 'wild-tome-1.appspot.com',
+  locationId: 'asia-south1',
+  apiKey: 'AIzaSyCv79ZF7X07IaFp-gXsBBDiH7yRhTnVBzM',
+  authDomain: 'wild-tome-1.firebaseapp.com',
+  messagingSenderId: '958141639770',
+});
