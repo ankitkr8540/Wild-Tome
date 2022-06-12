d961f60b8b20c7f25082243c63c38fe666c4356c
Ankit kumar <ankitkr8540@gmail.com>
2022-06-12 14:29:32 +0530

    changes done

--- frontend/src/screens/Home.js
+++ frontend/src/screens/Home.js
@@ -3,19 +3,19 @@ import FormContainer from '../components/FormContainer'
 import { Swiper, SwiperSlide } from 'swiper/react'
 const Home = () => {
 
-    // var swiper = new Swiper('.blog-slider', {
-    //     spaceBetween: 30,
-    //     effect: 'fade',
-    //     loop: true,
-    //     mousewheel: {
-    //         invert: false,
-    //     },
-    //     // autoHeight: true,
-    //     pagination: {
-    //         el: '.blog-slider__pagination',
-    //         clickable: true,
-    //     }
-    // });
+    var swiper = new Swiper('.blog-slider', {
+        spaceBetween: 30,
+        effect: 'fade',
+        loop: true,
+        mousewheel: {
+            invert: false,
+        },
+        // autoHeight: true,
+        pagination: {
+            el: '.blog-slider__pagination',
+            clickable: true,
+        }
+    });
     return (
         <>
             <div className='d-flex'>
